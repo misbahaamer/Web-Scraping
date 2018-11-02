de1d1190f591308d9f38219fad3c79891d577676
Misbah Aamer <misbahuddin.aamer@gmail.com>
2018-11-01 21:52:05 -0400

    added writeline

--- plantplaceswebservices/plantplaceswebservices/Program.cs
+++ plantplaceswebservices/plantplaceswebservices/Program.cs
@@ -10,6 +10,8 @@ namespace plantplaceswebservices
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("bismillah");
+            Console.Read();
         }
     }
 }
